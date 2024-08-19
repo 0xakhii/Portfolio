43f146c7aa84d73e3f496d3bdeca767e332c115e
akhi <ojamal@student.1337.ma>
2024-08-19 15:59:10 +0100

    initial commit

---
+++ my-portfolio/src/components/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>Email: your-email@example.com</p>
+      <p>LinkedIn: your-linkedin-profile</p>
+    </section>
+  );
+}
+
+export default Contact;
