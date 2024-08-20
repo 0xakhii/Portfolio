8b45040426f6919dc58ded9b94a47ccf52254e17
akhi <ojamal@student.1337.ma>
2024-08-20 14:13:37 +0100

    improvement

--- script.js
+++ script.js
@@ -44,22 +44,6 @@ function animate() {
 window.addEventListener("resize", function() {
     canvas.width = this.innerWidth, canvas.height = this.innerHeight, canvas.radius = canvas.height / 100 * (canvas.height / 100), init()
 }), init(), animate();
-
-function calculateAge(e) {
-    let t = new Date,
-        a = new Date(e);
-    if (a > t) return "Invalid";
-    if (1990 > a.getFullYear() || a.getFullYear() > 2010) return "Birthdate out of range";
-    let n = t.getFullYear() - a.getFullYear(),
-        l = t.getMonth() - a.getMonth();
-    return (l < 0 || 0 === l && t.getDate() < a.getDate()) && n--, n
-}
-document.addEventListener("DOMContentLoaded", () => {
-    let e = document.getElementById("birthday"),
-        t = e.getAttribute("datetime"),
-        a = calculateAge(t);
-    document.getElementById("age").textContent = a
-});
 "use strict";
 const elementToggleFunc = function(e) {
         e.classList.toggle("active")
