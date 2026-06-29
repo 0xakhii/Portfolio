494154716c5293e0b9195643297a8a4cf70e1d9d
akhi <ojamal@student.1337.ma>
2026-06-29 18:06:09 +0100

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,11 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    tanstackStart(),
+    tanstackStart({
+      spa: {
+        enabled: true,
+      },
+    }),
     react(),
     tailwindcss(),
   ],
